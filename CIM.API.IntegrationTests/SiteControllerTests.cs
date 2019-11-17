2b98b451300f53bb837811eda6960700cc8b87a2
Suksan Interaxsa <i.suksan@gmail.com>
2019-11-17 20:54:14 +0700

    Use UseInMemoryDatabase

---
+++ CIM.API.IntegrationTests/SiteControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIM.API.IntegrationTests
+{
+    public class SiteControllerTests : IntegrationTest
+    {
+
+        public async Task Get_Test()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+    }
+}
