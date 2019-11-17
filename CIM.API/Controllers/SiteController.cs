1dc4eb99881c341ef7ca88ec3331c014cefb5959
Suksan Interaxsa <i.suksan@gmail.com>
2019-11-17 11:52:18 +0700

    Simple get data from database

---
+++ CIM.API/Controllers/SiteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIM.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class SiteController : ControllerBase
+    {
+        public SiteController()
+        {
+
+        }
+        [HttpGet]
+        public int[] Get()
+        {
+            using(var ctx = new cim_dbContext())
+            {
+                var output = ctx.Sites.Select(x => x.Id).ToArray();
+                return output;
+            }
+        }
+    }
+}
