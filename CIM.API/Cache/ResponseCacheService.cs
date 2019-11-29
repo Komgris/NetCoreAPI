981448e0030eb70c65ee8e4a335939e6ee47c6d2
Suksan Interaxsa <i.suksan@gmail.com>
2019-11-29 22:44:09 +0700

    Merge branch 'feature/user-mng' of https://bitbucket.org/TOnns/cim_info_svc into feature/redis # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ CIM.API/Cache/ResponseCacheService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIM.API.Cache
+{
+    public class ResponseCacheService : IResponseCacheService
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ResponseCacheService(
+            IDistributedCache distributedCache
+            )
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<string> GetAsync(string key)
+        {
+            return await _distributedCache.GetStringAsync(key);
+        }
+
+        public async Task SetAsync(string key, object model)
+        {
+            var stringJson = JsonConvert.SerializeObject(model);
+            await _distributedCache.SetStringAsync(key, stringJson);
+        }
+    }
+}
