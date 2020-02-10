a26425d68ac827cf2708f515511528c2972c991e
Atiya Pichiannawakul <sriwisai.a@gmail.com>
2020-02-10 14:28:09 +0700

    Update model

--- CIM.Domain/Models/Material.cs
+++ CIM.Domain/Models/Material.cs
@@ -17,8 +17,8 @@ namespace CIM.Domain.Models
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime CreatedBy { get; set; }
+        public string CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public DateTime? UpdatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
     }
 }
