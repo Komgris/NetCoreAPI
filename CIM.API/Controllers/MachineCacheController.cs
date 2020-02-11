6a17a1d1362d5ed4ebceb51b82076940adc53f90
Suksan Interaxsa <i.suksan@gmail.com>
2020-02-11 21:31:25 +0700

    Add cache features

---
+++ CIM.API/Controllers/MachineCacheController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.API.Cache;
+using CIM.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineCacheController : ControllerBase
+    {
+        private IResponseCacheService _responseCacheService;
+
+        public MachineCacheController(
+            IResponseCacheService responseCacheService
+            )
+        {
+            _responseCacheService = responseCacheService;
+        }
+
+        //const string prefix = "machine";
+        //const string machineListKey = "machine-list";
+
+        [HttpGet]
+        public async Task<string> Get(int machineId)
+        {
+            var cacheKey = $"{Constans.RedisKey.MACHINE}{machineId}";
+            var cached = await _responseCacheService.GetAsync(cacheKey);
+            return cached;
+        }
+
+        [HttpPost]
+        public async Task<string> Add(int machineId, string data)
+        {
+            var machineList = await _responseCacheService.GetAsync(Constans.RedisKey.MACHINE_LIST) ?? "";
+            var distinctArray = machineList.Split(',')
+                .ToList();
+            distinctArray.Add(machineId.ToString());
+            var finalList = string.Join(',', distinctArray.Distinct());
+            var cacheKey = $"{Constans.RedisKey.MACHINE}{machineId}";
+            await _responseCacheService.SetAsync(Constans.RedisKey.MACHINE_LIST, finalList);
+            await _responseCacheService.SetAsync(cacheKey, data);
+            return "OK";
+        }
+
+
+    }
+}
---
+++ CIM.API/Controllers/MachineController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.API.Cache;
+using CIM.API.HubConfig;
+using CIM.BusinessLogic.Services;
+using CIM.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class MachineController : ControllerBase
+    {
+        private IHubContext<MachineHub> _hub;
+        private IResponseCacheService _responseCacheService;
+
+        public MachineController(IHubContext<MachineHub> hub,
+            IResponseCacheService responseCacheService)
+        {
+            _hub = hub;
+            _responseCacheService = responseCacheService;
+        }
+
+        public IActionResult Get()
+        {
+            var timerManager = new TimerService(() =>  _hub.Clients.All.SendAsync("transfermachinedata", DataService.GetData()));
+
+            return Ok(new { Message = "Request Machine Completed" });
+        }
+
+        //public async Task< List<MachineCacheModel>> GetMachineData()
+        //{
+        //    var machineList = (await _responseCacheService.GetAsync(machineListKey)).Split(',');
+        //    var mashineStatusData = new List<MachineCacheModel>();
+        //    foreach (var item in machineList)
+        //    {
+        //        var mcKey = $"cache:{prefix}:{item}";
+        //        var data = await _responseCacheService.GetAsync(mcKey);
+        //        mashineStatusData.Add(new MachineCacheModel
+        //        {
+        //            MachineId = item,
+        //            Data = data
+        //        });
+        //    }
+        //    return mashineStatusData;
+
+        //}
+
+    }
+}
