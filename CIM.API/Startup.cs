269244fe64c33a0c95b5bc9c825daa0dc19d430f
Atiya Pichiannawakul <sriwisai.a@gmail.com>
2020-03-06 21:29:58 +0700

    delete IPlanservice

--- CIM.API/Startup.cs
+++ CIM.API/Startup.cs
@@ -47,10 +47,9 @@ namespace CIM.API
             services.AddTransient<IUserAppTokenRepository, UserAppTokenRepository>();
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<IDirectSqlRepository, DirectSqlRepository>();
-            services.AddTransient<IPlanRepository, PlanRepository>();
+            services.AddTransient<IProductionPlanRepository, ProductionPlanRepository>();
             services.AddTransient<IMaterialRepository, MaterialRepository>();
 
-            services.AddTransient<IPlanService, PlanService>();
             services.AddTransient<IDirectSqlService, DirectSqlService>();
             services.AddTransient<ICipherService, CipherService>();
             services.AddTransient<ISiteService, SiteService>();
