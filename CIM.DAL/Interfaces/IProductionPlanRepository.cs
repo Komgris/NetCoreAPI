7ced7646b3b3c6e63680194bd370aff701074622
Atiya Pichiannawakul <sriwisai.a@gmail.com>
2020-03-07 11:48:40 +0700

    add file ProductionPlanRepository and IProductionPlanRepository for fixed missing file after pull from development

---
+++ CIM.DAL/Interfaces/IProductionPlanRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CIM.Domain.Models;
+using CIM.Model;
+
+namespace CIM.DAL.Interfaces
+{
+    public interface IProductionPlanRepository : IRepository<ProductionPlan>
+    {
+        Task<PagingModel<ProductionPlanModel>> Paging(int page, int howmany);
+        List<ProductionPlanModel> Get();
+        void InsertProduction(List<ProductionPlanModel> import);
+        void DeleteProduction(string id);
+        void UpdateProduction(List<ProductionPlanModel> list);
+    }
+
+}
