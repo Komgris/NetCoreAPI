66d0c42748dbfddc99a3fda478084ec18668a368
Atiya Pichiannawakul <sriwisai.a@gmail.com>
2020-03-22 14:17:21 +0700

    add model

---
+++ CIM.Model/MachineModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.Model
+{
+    public class MachineModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StatusId { get; set; }
+        public int MachineTypeId { get; set; }
+        public string Plcaddress { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+
+        public MachineTypeModel MachineType { get; set; }
+        public MachineStatusModel Status { get; set; }
+    }
+}
---
+++ CIM.Model/MachineStatusModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.Model
+{
+    public class MachineStatusModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
