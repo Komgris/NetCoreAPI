9aa7adf9e6b2fa596042b4bb6828a80b0480ffe6
isuksn <i.suksan@gmail.com>
2020-04-07 22:43:56 +0700

    Add service

---
+++ CIM.BusinessLogic/Services/LossRecordService.cs
@@ -0,0 +1,19 @@
+﻿using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIM.BusinessLogic.Services
+{
+    public class LossRecordService : BaseService, ILossRecordService
+    {
+
+        public Task Create(ActiveProcessModel activeProcess)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
