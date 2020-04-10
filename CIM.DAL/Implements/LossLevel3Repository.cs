1f80105ba6c910cdf5cb904278d1e392d5590a97
isuksn <i.suksan@gmail.com>
2020-04-11 00:10:45 +0700

    Add comment

--- CIM.DAL/Implements/LossLevel3Repository.cs
+++ CIM.DAL/Implements/LossLevel3Repository.cs
@@ -19,6 +19,7 @@ namespace CIM.DAL.Implements
         public async Task<IList<LossLevelComponentMappingModel>> ListComponentMappingAsync()
         {
             IList<LossLevelComponentMappingModel> data = null;
+            //If you get a Object reference not set to instance of object error then make sure your sp result column is not null
             var proc = _entities.LoadStoredProc("sp_ListComponentMappingAsync");
             await proc.ExecAsync(x => Task.Run(() => data = x.ToList<LossLevelComponentMappingModel>()));
             return data;
@@ -29,6 +30,7 @@ namespace CIM.DAL.Implements
         {
 
             IList<LossLevelMachineMappingModel> data = null;
+            //If you get a Object reference not set to instance of object error then make sure your sp result column is not null
             var proc = _entities.LoadStoredProc("sp_ListMachineMappingAsync");
             await proc.ExecAsync(x => Task.Run(() => data = x.ToList<LossLevelMachineMappingModel>()));
             return data;
