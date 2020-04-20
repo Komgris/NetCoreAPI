48194cbd8f67a5d8be3a866aea130d2bc6d64370
uthai <uthai.psec@gmail.com>
2020-04-20 15:18:43 +0700

    lv 3 controller befor clean code

---
+++ CIM.API/Controllers/LossLevel3Controller.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text.Json;
+//using System.Threading.Tasks;
+using CIM.API.HubConfig;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LossLevel3Controller : ControllerBase
+    {
+
+        private ILossLevel3Service _service;
+        public LossLevel3Controller(
+            //IHubContext<MachineHub> hub,
+            //IProductionPlanService productionPlanService,
+            ILossLevel3Service service
+        )
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/List")]
+        public async Task<ProcessReponseModel<PagingModel<LossLevel3ViewModel>>> List(string keyword = "", int page = 1, int howmany = 10)
+        {
+            var output = new ProcessReponseModel<PagingModel<LossLevel3ViewModel>>();
+            try
+            {
+                // todo
+                output.Data = await _service.List(keyword, page, howmany);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.ToString();
+            }
+            return output;
+        }
+
+        //// GET: api/LossLevel3
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/LossLevel3/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/LossLevel3
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/LossLevel3/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
