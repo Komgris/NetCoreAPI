18f62761085d7a86ccd6c58f8c999308e0fd4fa3
uthai <uthai.psec@gmail.com>
2020-04-20 15:16:50 +0700

    view model lv3

---
+++ CIM.Model/LossLevel3ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.Model
+{
+    public class LossLevel3ViewModel : LossLevel3Model
+    {
+        public int LossLevel1Id { get; set; }
+        public string LossLevel1Name { get; set; }
+        public string LossLevel2Name { get; set; }
+    }
+}
