95f386307a78de84632c9fd21a11c0ae746559a1
uthai <uthai.psec@gmail.com>
2020-04-22 17:05:20 +0700

    clean code

--- CIM.DAL/Implements/LossLevel3Repository.cs
+++ CIM.DAL/Implements/LossLevel3Repository.cs
@@ -50,63 +50,10 @@ namespace CIM.DAL.Implements
             return data;
         }
 
-        //public async Task<PagingModel<LossLevel3ViewModel>> ListAsPaging(int page, int howmany, string keyword, bool isActive)
-        //{
-        //    List<LossLevel3ViewModel> data = null;
-        //    var proc = _entities.LoadStoredProc("[sp_ListLossLevel3]");
-        //    proc.AddParam("total_count", out IOutParam<int> totalCount);
-        //    proc.AddParam("@keyword", keyword);
-        //    proc.AddParam("@is_active", isActive);
-        //    proc.AddParam("@howmany", howmany);
-        //    proc.AddParam("@page", page);
-        //    await proc.ExecAsync(x => Task.Run(() => data = x.ToList<LossLevel3ViewModel>()));
-        //    return ToPagingModel(data, totalCount.Value, page, howmany);
-        //}
-
         public async Task<PagingModel<LossLevel3ViewModel>> ListAsPaging(int page, int howmany, string keyword, bool isActive)
         {
             List<LossLevel3ViewModel> output = new List<LossLevel3ViewModel>();
-
             int total = 0;
-
-
-            //using (var connection = new SqlConnection(_connectionString))
-            //{
-            //    string sql = @"EXEC	[dbo].[sp_ListLossLevel3] @is_active = 1";
-            //    var output = connection.QueryAsync<LossLevel3ViewModel>(sql).Result.ToList();
-            //    Console.WriteLine(output.Count());
-            //}
-
-            //using (var connection = new SqlConnection(_connectionString))
-            //{
-            //    DynamicParameters parameter = new DynamicParameters();
-            //    parameter.Add("@keyword", keyword, DbType.String, ParameterDirection.Input);
-            //    parameter.Add("@is_active", isActive, DbType.Boolean, ParameterDirection.Input);
-            //    parameter.Add("@page", page, DbType.Int16, ParameterDirection.Input);
-            //    parameter.Add("@howmany", howmany, DbType.Int16, ParameterDirection.Input);
-            //    string sql = @"sp_ListLossLevel3_cte";
-            //    var result = connection.QueryAsync(sql, parameter, null, null, CommandType.StoredProcedure).Result.ToList();
-
-            //    Console.WriteLine(output.Count());
-
-
-            //    foreach (var item in result)
-            //    {
-            //        output.Add(new LossLevel3ViewModel
-            //        {
-            //            Id = Convert.ToInt16(item.Id),
-            //            Name = Convert.ToString(item.Name),
-            //            Description = Convert.ToString(item.Description),
-            //            IsActive = Convert.ToBoolean(item.IsActive),
-            //            LossLevel2Id = Convert.ToInt16(item.LossLevel2Id),
-            //            LossLevel1Id = Convert.ToInt16(item.LossLevel1Id),
-            //            LossLevel1Name = Convert.ToString(item.LossLevel1Name),
-            //            LossLevel2Name = Convert.ToString(item.LossLevel2Name),
-
-            //        });
-            //    }
-            //}
-
             DynamicParameters parameter = new DynamicParameters();
             parameter.Add("@keyword", keyword, DbType.String, ParameterDirection.Input);
             parameter.Add("@is_active", isActive, DbType.Boolean, ParameterDirection.Input);
@@ -135,7 +82,6 @@ namespace CIM.DAL.Implements
                 });
             }
 
-            
             return new PagingModel<LossLevel3ViewModel>
             {
                 HowMany = total,
