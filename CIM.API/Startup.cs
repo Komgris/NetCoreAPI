0ed88e9a3a8da2e39cb6102d8c5b55c3abf3c754
uthai <uthai.psec@gmail.com>
2020-04-24 04:41:40 +0700

    no message

--- CIM.API/Startup.cs
+++ CIM.API/Startup.cs
@@ -77,7 +77,6 @@ namespace CIM.API
 
             services.AddTransient<IMasterDataService, MasterDataService>();
             services.AddTransient<IReportService, ReportService>();
-            services.AddTransient<ILossLevel3Service, LossLevel3Service>();
             services.AddControllers();
             services.AddSignalR();
             services.AddSwaggerGen(c =>
