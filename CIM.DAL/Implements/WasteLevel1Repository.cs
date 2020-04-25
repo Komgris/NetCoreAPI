2c92f3f9cce9927c7a6172ec8554f8cd4f9182ec
isuksn <i.suksan@gmail.com>
2020-04-25 13:03:14 +0700

    Add Repo and domain models

---
+++ CIM.DAL/Implements/WasteLevel1Repository.cs
@@ -0,0 +1,17 @@
+﻿using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.DAL.Implements
+{
+    public class WasteLevel1Repository : Repository<WasteLevel1>, IWasteLevel1Repository
+    {
+        public WasteLevel1Repository(cim_dbContext context, IConfiguration configuration) : base(context, configuration)
+        {
+
+        }
+    }
+}
---
+++ CIM.DAL/Implements/WasteLevel2Repository.cs
@@ -0,0 +1,17 @@
+﻿using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.DAL.Implements
+{
+    public class WasteLevel2Repository : Repository<WasteLevel2>, IWasteLevel2Repository
+    {
+        public WasteLevel2Repository(cim_dbContext context, IConfiguration configuration) : base(context, configuration)
+        {
+
+        }
+    }
+}
