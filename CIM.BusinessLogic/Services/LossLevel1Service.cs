9c1129369ea5b5f570415e56d5849e5752a40513
uthai <uthai.psec@gmail.com>
2020-04-25 16:56:51 +0700

    lv1 lv2

---
+++ CIM.BusinessLogic/Services/LossLevel1Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CIM.BusinessLogic.Interfaces;
+using CIM.BusinessLogic.Utility;
+using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using CIM.Model;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIM.BusinessLogic.Services
+{
+    public class LossLevel1Service : BaseService, ILossLevel1Service
+    {
+        private readonly ILossLevel1Repository _lossLevel1Repository;
+        private IUnitOfWorkCIM _unitOfWork;
+
+        public LossLevel1Service(
+            IUnitOfWorkCIM unitOfWork,
+            ILossLevel1Repository lossLevel1Repository
+            )
+        {
+            _lossLevel1Repository = lossLevel1Repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PagingModel<Model.LossLevel1Model>> List(string keyword, int page, int howmany, bool isActive)
+        {
+            var output = await _lossLevel1Repository.List(page, howmany, keyword, isActive);
+            return output;
+        }
+    }
+}
