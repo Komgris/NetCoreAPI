7b434e742a4bff83a4983e8c214b0c3028fd0de9
isuksn <i.suksan@gmail.com>
2020-04-25 14:50:27 +0700

    Add dictionary model

--- CIM.Model/MasterDataModel.cs
+++ CIM.Model/MasterDataModel.cs
@@ -7,12 +7,12 @@ namespace CIM.Model
     public class MasterDataModel
     {
 
-        public IDictionary<int, LossLevel3Model> LossLevel3s { get; set; }
+        public IDictionary<int, LossLevel3DictionaryModel> LossLevel3s { get; set; }
         public IDictionary<int, MachineComponentModel> Components { get; set; }
         public IDictionary<int, MachineModel> Machines { get; set; }
         public IDictionary<int, int[]> RouteMachines { get; set; }
         public IDictionary<int, RouteModel> Routes { get; set; }
-        public IDictionary<string, ProductionPlanModel> ProductionPlan { get; set; }
+        public IDictionary<string, ProductionPlanDictionaryModel> ProductionPlan { get; set; }
 
         public IDictionary<int, IDictionary<int,string>> ProductGroupRoutes { get; set; }
 
