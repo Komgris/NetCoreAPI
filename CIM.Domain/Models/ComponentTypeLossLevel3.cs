ce4dd97055d8f4e38a26c9af73eea021f3ce6a9c
isuksn <i.suksan@gmail.com>
2020-05-01 09:23:51 +0700

    update database change

--- CIM.Domain/Models/ComponentTypeLossLevel3.cs
+++ CIM.Domain/Models/ComponentTypeLossLevel3.cs
@@ -6,10 +6,10 @@ namespace CIM.Domain.Models
     public partial class ComponentTypeLossLevel3
     {
         public int Id { get; set; }
-        public int MachineComponentTypeId { get; set; }
+        public int ComponentTypeId { get; set; }
         public int LossLevel3Id { get; set; }
 
+        public virtual ComponentType ComponentType { get; set; }
         public virtual LossLevel3 LossLevel3 { get; set; }
-        public virtual ComponentType MachineComponentType { get; set; }
     }
 }
