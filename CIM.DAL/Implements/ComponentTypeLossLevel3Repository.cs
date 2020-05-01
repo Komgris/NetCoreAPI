498346c8f1ecf375a363ef03cc55809ace48c653
uthai <uthai.psec@gmail.com>
2020-05-01 19:09:13 +0700

    repository

---
+++ CIM.DAL/Implements/ComponentTypeLossLevel3Repository.cs
@@ -0,0 +1,24 @@
+﻿using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using CIM.Model;
+using StoredProcedureEFCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using CIM.DAL.Utility;
+
+namespace CIM.DAL.Implements
+{
+    public class ComponentTypeLossLevel3Repository : Repository<ComponentTypeLossLevel3>, IComponentTypeLossLevel3Repository
+    {
+        private IDirectSqlRepository _directSqlRepository;
+
+        public ComponentTypeLossLevel3Repository(cim_dbContext context, IDirectSqlRepository directSqlRepository, IConfiguration configuration) : base(context, configuration)
+        {
+            _directSqlRepository = directSqlRepository;
+        }
+
+    }
+}
---
+++ CIM.DAL/Implements/MachineTypeLossLevel3Repository.cs
@@ -0,0 +1,23 @@
+﻿using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using CIM.Model;
+using StoredProcedureEFCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using CIM.DAL.Utility;
+
+namespace CIM.DAL.Implements
+{
+    public class MachineTypeLossLevel3Repository : Repository<MachineTypeLossLevel3>, IMachineTypeLossLevel3Repository
+    {
+        private IDirectSqlRepository _directSqlRepository;
+
+        public MachineTypeLossLevel3Repository(cim_dbContext context, IDirectSqlRepository directSqlRepository, IConfiguration configuration) : base(context, configuration)
+        {
+            _directSqlRepository = directSqlRepository;
+        }
+    }
+}
