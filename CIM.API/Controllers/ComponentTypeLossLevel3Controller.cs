83f2184045b9024ea647cd6dc46bcb74575ede65
uthai <uthai.psec@gmail.com>
2020-05-01 19:08:34 +0700

    controller mock

---
+++ CIM.API/Controllers/ComponentTypeLossLevel3Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+
+namespace CIM.API.Controllers
+{
+    [ApiController]
+    public class ComponentTypeLossLevel3Controller : ControllerBase
+    {
+        private IComponentTypeLossLevel3Service _service;
+        public ComponentTypeLossLevel3Controller(
+            IComponentTypeLossLevel3Service service
+        )
+        {
+            _service = service;
+        }
+    }
+}
---
+++ CIM.API/Controllers/MachineTypeLossLevel3Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+
+namespace CIM.API.Controllers
+{
+    [ApiController]
+    public class MachineTypeLossLevel3Controller : ControllerBase
+    {
+        private IMachineTypeLossLevel3Service _service;
+        public MachineTypeLossLevel3Controller(
+            IMachineTypeLossLevel3Service service
+        )
+        {
+            _service = service;
+        }
+
+    }
+}
