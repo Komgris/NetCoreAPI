5717d01601aa3669a0092575eda5a638388557f8
devspsec <devs.psec@gmail.com>
2020-05-04 14:24:28 +0700

    Create machinetype repo and service

--- CIM.Model/MachineTypeModel.cs
+++ CIM.Model/MachineTypeModel.cs
@@ -8,6 +8,8 @@ namespace CIM.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Image { get; set; }
+        public bool HasOee { get; set; }
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
         public DateTime CreatedAt { get; set; }
