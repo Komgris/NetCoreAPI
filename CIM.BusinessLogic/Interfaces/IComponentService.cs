326e5e1c23c9912bf675685f57051244554117d5
devspsec <devs.psec@gmail.com>
2020-05-07 12:33:23 +0700

    Add interface

---
+++ CIM.BusinessLogic/Interfaces/IComponentService.cs
@@ -0,0 +1,14 @@
+﻿using CIM.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIM.BusinessLogic.Interfaces
+{
+    public interface IComponentService
+    {
+        Task<List<ComponentModel>> GetComponentByMachine(int machineId);
+        Task<PagingModel<ComponentModel>> List(string keyword, int page, int howmany);
+    }
+}
