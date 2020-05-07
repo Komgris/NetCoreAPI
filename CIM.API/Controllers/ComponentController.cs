a0912a646b9529d8434bf8c949355ddac843c0d4
devspsec <devs.psec@gmail.com>
2020-05-07 14:14:56 +0700

    Create Component-API

---
+++ CIM.API/Controllers/ComponentController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentController : BaseController
+    {
+        private IResponseCacheService _responseCacheService;
+        private IComponentService _componentService;
+
+        public ComponentController(
+        IResponseCacheService responseCacheService,
+        IComponentService componentService
+    )
+        {
+            _responseCacheService = responseCacheService;
+            _componentService = componentService;
+        }
+
+
+        [Route("api/[controller]/GetByMachine")]
+        [HttpGet]
+        public async Task<ProcessReponseModel<List<ComponentModel>>> GetByMachineType(int machineId)
+        {
+            var output = new ProcessReponseModel<List<ComponentModel>>();
+            try
+            {
+                output.Data = await _componentService.GetComponentByMachine(machineId);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+            return output;
+        }
+
+        [Route("api/[controller]/List")]
+        [HttpGet]
+        public async Task<ProcessReponseModel<PagingModel<ComponentModel>>> List(string keyword = "", int page = 1, int howmany = 10)
+        {
+            var output = new ProcessReponseModel<PagingModel<ComponentModel>>();
+            try
+            {
+                output.Data = await _componentService.List(keyword, page, howmany);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+            return output;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/Get")]
+        public async Task<ProcessReponseModel<ComponentModel>> Get(int id)
+        {
+            var output = new ProcessReponseModel<ComponentModel>();
+            try
+            {
+                // todo
+                //var currentUser = (CurrentUserModel)HttpContext.Items[Constans.CURRENT_USER];
+                output.Data = await _componentService.Get(id);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+            return output;
+        }
+
+        [Route("api/[controller]/Create")]
+        [HttpPost]
+        public async Task<ProcessReponseModel<ComponentModel>> Create([FromBody] ComponentModel data)
+        {
+            var output = new ProcessReponseModel<ComponentModel>();
+            try
+            {
+                await _componentService.Create(data);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+            return output;
+        }
+
+        [Route("api/[controller]/Update")]
+        [HttpPut]
+        public async Task<ProcessReponseModel<ComponentModel>> Update([FromBody] ComponentModel data)
+        {
+            var output = new ProcessReponseModel<ComponentModel>();
+            try
+            {
+                await _componentService.Update(data);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+            return output;
+        }
+    }
+}
--- CIM.BusinessLogic/Interfaces/IComponentService.cs
+++ CIM.BusinessLogic/Interfaces/IComponentService.cs
@@ -10,5 +10,8 @@ namespace CIM.BusinessLogic.Interfaces
     {
         Task<List<ComponentModel>> GetComponentByMachine(int machineId);
         Task<PagingModel<ComponentModel>> List(string keyword, int page, int howmany);
+        Task<ComponentModel> Get(int id);
+        Task Update(ComponentModel data);
+        Task Create(ComponentModel data);
     }
 }
