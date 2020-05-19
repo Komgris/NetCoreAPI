49d60dbca114d6676dedac4ea8894e9d6a95395c
Ferine <sriwisai.a@gmail.com>
2020-05-19 22:55:12 +0700

    fix case sensitive keyword when search component

--- CIM.BusinessLogic/Services/ComponentService.cs
+++ CIM.BusinessLogic/Services/ComponentService.cs
@@ -103,7 +103,7 @@ namespace CIM.BusinessLogic.Services
         public async Task<List<ComponentModel>> GetComponentNoMachineId(string keyword)
         {
             var dbModels = await _componentRepository.WhereAsync(x => x.MachineId == null);
-            var components = dbModels.Where(x=> string.IsNullOrEmpty(keyword) ? true : (x.Name.Contains(keyword)))
+            var components = dbModels.Where(x=> string.IsNullOrEmpty(keyword) ? true : (x.Name.ToLower().Contains(keyword.ToLower())))
                 .Select(
                     x => new ComponentModel
                     {
