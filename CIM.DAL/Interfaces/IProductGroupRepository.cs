6f94dae566edb919447d75457cf8bf4ea93a8405
devspsec <devs.psec@gmail.com>
2020-05-19 22:05:45 +0700

    create repo product group

---
+++ CIM.DAL/Interfaces/IProductGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using CIM.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIM.DAL.Interfaces
+{
+    public interface IProductGroupRepository : IRepository<ProductGroup>
+    {
+        Task<List<RouteProductGroup>> ListRouteByProductGroup(int productGroupId);
+    }
+}
---
+++ CIM.Model/RouteProductGroupModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIM.Model
+{
+    public class RouteProductGroupModel
+    {
+        public int Id { get; set; }
+        public int RouteId { get; set; }
+        public int ProductGroupId { get; set; }
+        public bool? IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
