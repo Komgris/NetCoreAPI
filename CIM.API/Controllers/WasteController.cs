64ef4d00ab557a6302243a75e26c262e02017817
isuksn <i.suksan@gmail.com>
2020-06-08 11:30:31 +0700

    Add missing files

---
+++ CIM.API/Controllers/WasteController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WasteController : BaseController
+    {
+        private IRecordProductionPlanWasteService _recordProductionPlanWasteService;
+
+        public WasteController(
+            IRecordProductionPlanWasteService recordProductionPlanWasteService
+            )
+        {
+            _recordProductionPlanWasteService = recordProductionPlanWasteService;
+        }
+
+        [HttpPost]
+        public async Task<ProcessReponseModel<RecordProductionPlanWasteModel>> Create(RecordProductionPlanWasteModel model)
+        {
+            var output = new ProcessReponseModel<RecordProductionPlanWasteModel>();
+            try
+            {
+                output.Data = await _recordProductionPlanWasteService.Create(model);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+
+            return output;
+        }
+
+    }
+}
