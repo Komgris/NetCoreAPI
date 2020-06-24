235f503e59e40dcc7c604ae9b7460721f0a0508c
Ferine <sriwisai.a@gmail.com>
2020-06-24 23:13:34 +0700

    update machineteam db

--- CIM.Domain/Models/cim_dbContext.cs
+++ CIM.Domain/Models/cim_dbContext.cs
@@ -908,7 +908,6 @@ namespace CIM.Domain.Models
                 entity.HasOne(d => d.Team)
                     .WithMany(p => p.MachineTeam)
                     .HasForeignKey(d => d.TeamId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_Machine_Team_Team");
             });
 
