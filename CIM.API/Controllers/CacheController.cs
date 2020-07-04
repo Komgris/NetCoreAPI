2ab19c1f1e519559ac3595c58136bcb8413736eb
isuksn <i.suksan@gmail.com>
2020-07-04 14:24:32 +0700

    export cached plans

--- CIM.API/Controllers/CacheController.cs
+++ CIM.API/Controllers/CacheController.cs
@@ -1,19 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CIM.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CacheController : ControllerBase
+    public class CacheController : BaseController
     {
+        private IActiveProductionPlanService _activeProductionPlanService;
+        private IMachineService _machineService;
         private IResponseCacheService _responseCacheService;
 
         public CacheController(
+            IActiveProductionPlanService activeProductionPlanService,
+            IMachineService machineService,
             IResponseCacheService responseCacheService
             )
         {
+            _activeProductionPlanService = activeProductionPlanService;
+            _machineService = machineService;
             _responseCacheService = responseCacheService;
         }
 
@@ -32,6 +41,66 @@ namespace CIM.API.Controllers
             return "OK";
         }
 
+        [HttpGet]
+        [Route("GetProductionPlans")]
+        public async Task<string> GetProductionPlan(string planIds)
+        {
+
+            var planList = planIds.Split(',');
+            var exportModel = new CacheExportModel();
+            try
+            {
+                foreach (var planId in planList)
+                {
+                    var plan = await _activeProductionPlanService.GetCached(planId);
+                    if (plan != null)
+                    {
+                        foreach (var route in plan?.ActiveProcesses)
+                        {
+                            foreach (var machine in route.Value.Route.MachineList)
+                            {
+                                exportModel.Machines.Add(machine.Value);
+                            }
+                        }
+
+                        exportModel.ProductionPlans.Add(plan);
+                    }
+                }
+
+                return JsonConvert.SerializeObject(exportModel, JsonsSetting);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("SetProductionPlans")]
+        public async Task<ResponseModel<CacheExportModel>> SetProductionPlans(CacheExportModel model)
+        {
+
+            var output = new ResponseModel<CacheExportModel>();
+            try
+            {
+                foreach (var plan in model.ProductionPlans)
+                {
+                    await _activeProductionPlanService.SetCached(plan);
+                }
+
+                foreach (var machine in model.Machines)
+                {
+                    await _machineService.SetCached(machine.Id,machine);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                output.ErrorMessage = ex.Message;
+            }
+            return output;
+        }
 
     }
 }
