7aabc925f9a72b88284d813c825d80e8b69c0af2
ToN NakuB <nprssp_tk@hotmail.com>
2020-07-07 16:25:03 +0700

    move counter&status to HW interface

--- CIM.API/Controllers/HardwareInterfaceController.cs
+++ CIM.API/Controllers/HardwareInterfaceController.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using CIM.API.HubConfig;
 using CIM.BusinessLogic.Interfaces;
 using CIM.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace CIM.API.Controllers
 {
     [ApiController]
-    public class HardwareInterfaceController : BaseController
-    {
+    public class HardwareInterfaceController : BoardcastController {
         private IMachineService _machineService;
 
         public HardwareInterfaceController(
-            IMachineService machineService
-            )
+            IHubContext<GlobalHub> hub,
+            IMachineService machineService,
+            IResponseCacheService responseCacheService,
+            IReportService service,
+            IConfiguration config,
+            IActiveProductionPlanService activeProductionPlanService
+            ) : base(hub, responseCacheService, service, config, activeProductionPlanService)
         {
             _machineService = machineService;
         }
@@ -81,6 +89,48 @@ namespace CIM.API.Controllers
             {
                 return ex.Message;
             }
+        }
+        
+        [HttpGet]
+        [Route("api/[controller]/SetStatus")]
+        public async Task<string> SetStatus(int id, int statusId, bool isAuto = true)
+        {
+            var productionPlan = await _activeProductionPlanService.UpdateByMachine(id, statusId, isAuto);
+
+            // Production plan of this component doesn't started yet
+            if (productionPlan != null)
+            {
+                var channelKey = $"{Constans.SIGNAL_R_CHANNEL_PRODUCTION_PLAN}-{productionPlan.ProductionPlanId}";
+                await HandleBoardcastingActiveProcess(Constans.BoardcastType.ActiveMachineInfo, productionPlan.ProductionPlanId
+                    , productionPlan.ActiveProcesses.Select(o => o.Key).ToArray(), productionPlan);
+            }
+            return "OK";
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/UpdateMachineProduceCounter")]
+        public async Task<ProcessReponseModel<object>> UpdateMachineProduceCounter([FromBody] List<MachineProduceCounterModel> listData, int? hour)
+        {
+            var hr = hour ?? DateTime.Now.Hour;
+            var output = new ProcessReponseModel<object>();
+            try
+            {
+                var productionPlans = await _activeProductionPlanService.UpdateMachineOutput(listData, hr);
+
+                foreach (var productionPlan in productionPlans)
+                {
+                    await HandleBoardcastingActiveProcess(Constans.BoardcastType.ActiveProductionSummary, productionPlan.ProductionPlanId
+                                                                , productionPlan.ActiveProcesses.Select(o => o.Key).ToArray(), productionPlan);
+                }
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.Message;
+            }
+
+            return output;
+
         }
     }
 }
