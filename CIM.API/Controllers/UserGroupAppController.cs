fb00bb69c090ce76d4c81c5829e53955f978d925
Ferine <sriwisai.a@gmail.com>
2020-07-17 19:08:08 +0700

    create user group app

---
+++ CIM.API/Controllers/UserGroupAppController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CIM.BusinessLogic.Interfaces;
+using CIM.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIM.API.Controllers
+{
+    [ApiController]
+    public class UserGroupAppController : ControllerBase
+    {
+        private IUserGroupAppService _service;
+
+        public UserGroupAppController(IUserGroupAppService service)
+        {
+            _service = service;
+        }
+
+        //[HttpPost]
+        //[Route("api/[controller]/Create")]
+        //public async Task<ProcessReponseModel<UserGroupModel>> Create(UserGroupModel model)
+        //{
+        //    var output = new ProcessReponseModel<UserGroupModel>();
+        //    try
+        //    {
+        //        await _service.Create(model);
+        //        output.IsSuccess = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        output.Message = ex.Message;
+        //    }
+        //    return output;
+        //}
+
+
+        //[HttpGet]
+        //[Route("api/[controller]/List")]
+        //public async Task<ProcessReponseModel<PagingModel<UserGroupModel>>> List(string keyword = "", int page = 1, int howMany = 10, bool isActive = true)
+        //{
+        //    var output = new ProcessReponseModel<PagingModel<UserGroupModel>>();
+        //    try
+        //    {
+        //        output.Data = await _service.List(keyword, page, howMany, isActive);
+        //        output.IsSuccess = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        output.Message = ex.ToString();
+        //    }
+        //    return output;
+        //}
+
+        [HttpGet]
+        [Route("api/[controller]/Get")]
+        public async Task<ProcessReponseModel<List<UserGroupAppModel>>> Get(int id)
+        {
+            var output = new ProcessReponseModel<List<UserGroupAppModel>>();
+            try
+            {
+                output.Data = await _service.Get(id);
+                output.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                output.Message = ex.ToString();
+            }
+            return output;
+        }
+
+        //[HttpPut]
+        //[Route("api/[controller]/Update")]
+        //public async Task<ProcessReponseModel<UserGroupModel>> Update([FromBody] UserGroupModel model)
+        //{
+        //    var output = new ProcessReponseModel<UserGroupModel>();
+        //    try
+        //    {
+        //        await _service.Update(model);
+        //        output.IsSuccess = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        output.Message = ex.Message;
+        //    }
+        //    return output;
+        //}
+
+    }
+}
---
+++ CIM.BusinessLogic/Services/UserGroupAppService.cs
@@ -0,0 +1,126 @@
+﻿using CIM.BusinessLogic.Interfaces;
+using CIM.BusinessLogic.Utility;
+using CIM.DAL.Interfaces;
+using CIM.Domain.Models;
+using CIM.Model;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CIM.BusinessLogic.Services
+{
+    public class UserGroupAppService : BaseService, IUserGroupAppService
+    {
+        private IUserGroupRepository _userGroupRepository;
+        private IUserGroupAppRepository _userGroupAppRepository;
+        private IUserGroupAppFeatureRepository _userGroupAppFeatureRepository;
+        private IUnitOfWorkCIM _unitOfWork;
+
+        public UserGroupAppService(
+            IUserGroupRepository userGroupRepository,
+            IUserGroupAppRepository userGroupAppRepository,
+            IUserGroupAppFeatureRepository userGroupAppFeatureRepository,
+            IUnitOfWorkCIM unitOfWork
+            )
+        {
+            _userGroupRepository = userGroupRepository;
+            _userGroupAppRepository = userGroupAppRepository;
+            _userGroupAppFeatureRepository = userGroupAppFeatureRepository;
+            _unitOfWork = unitOfWork;
+        }
+        //public async Task Create(UserGroupModel model)
+        //{
+        //    var dbModel = new UserGroups
+        //    {
+        //        Name = model.Name,
+        //        IsActive = true,
+        //        IsDelete = false
+        //    };
+        //    _userGroupRepository.Add(dbModel);
+        //    await _unitOfWork.CommitAsync();
+
+        //    await InsertAppAndFeatureMapping(model, dbModel.Id);
+        //}
+
+        //public async Task<PagingModel<UserGroupModel>> List(string keyword, int page, int howMany, bool isActive)
+        //{
+        //    var output = await _userGroupRepository.ListAsPaging("sp_ListUserGroup", new Dictionary<string, object>()
+        //        {
+        //            {"@keyword", keyword},
+        //            {"@howmany", howMany},
+        //            {"@page", page},
+        //            {"@is_active", isActive}
+        //        }, page, howMany);
+        //    return output;
+        //}
+
+        public async Task<List<UserGroupAppModel>> Get(int id)
+        {
+            var db_model = await _userGroupAppRepository.Where(x => x.UserGroupId == id)
+                .Select(x => new UserGroupAppModel
+                {
+                    Id = x.Id,
+                    AppId = x.AppId,
+                    AppName = x.App.Name,
+                    UserGroupId = x.UserGroupId
+                }).ToListAsync();
+            return db_model;
+        }
+
+        //public async Task Update(UserGroupModel model)
+        //{
+        //    var db_model = MapperHelper.AsModel(model, new UserGroups());
+        //    _userGroupRepository.Edit(db_model);
+        //    await _unitOfWork.CommitAsync();
+
+        //    await DeleteMapping(model.Id);
+        //    await InsertAppAndFeatureMapping(model, model.Id);
+        //}
+
+        //public async Task InsertAppAndFeatureMapping(UserGroupModel model, int userGroupId)
+        //{
+        //    foreach (var item in model.AppList)
+        //    {
+        //        var appDBModel = new UserGroupsApps
+        //        {
+        //            AppId = item.Id,
+        //            UserGroupId = userGroupId
+        //        };
+        //        _userGroupAppRepository.Add(appDBModel);
+        //        await _unitOfWork.CommitAsync();
+
+        //        var appUserGroupId = appDBModel.Id;
+
+        //        foreach (var feature in item.FeatureList)
+        //        {
+        //            var appFeatureDBModel = new UserGroupsAppFeatures
+        //            {
+        //                FeatureId = feature.FeatureId,
+        //                AppUserGroupId = appUserGroupId
+        //            };
+        //            _userGroupAppFeatureRepository.Add(appFeatureDBModel);
+        //            await _unitOfWork.CommitAsync();
+        //        }
+        //    }            
+        //}
+
+        //public async Task DeleteMapping(int userGroupId)
+        //{
+        //    var appList = await _userGroupAppRepository.WhereAsync(x => x.UserGroupId == userGroupId);
+        //    foreach (var item in appList)
+        //    {
+        //        var featureList = await _userGroupAppFeatureRepository.WhereAsync(x => x.AppUserGroupId == item.Id);
+        //        foreach (var featureItem in featureList)
+        //        {
+        //            _userGroupAppFeatureRepository.Delete(featureItem);
+        //        }
+        //        _userGroupAppRepository.Delete(item);
+        //    }
+        //}
+    }
+}
