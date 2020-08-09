ddc8a01759c83fd9729d82f0c26f5bad5bad2f2f
isuksn <i.suksan@gmail.com>
2020-08-09 21:39:28 +0700

    Add category

--- CIM.Model/AccidentModel.cs
+++ CIM.Model/AccidentModel.cs
@@ -7,6 +7,8 @@ namespace CIM.Model
     public class AccidentModel
     {
         public int Id { get; set; }
+        public int? CategoryId { get; set; }
+        public string Category { get; set; }
         public string Title { get; set; }
         public DateTime HappenAt { get; set; }
         public DateTime CreatedAt { get; set; }
