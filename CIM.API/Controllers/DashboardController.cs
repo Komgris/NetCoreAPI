378407ae6b3fd83e4230f84bda922cf4531303ea
ToN NakuB <nprssp_tk@hotmail.com>
2020-08-12 10:22:15 +0700

    add dashboard ctrl

---
+++ CIM.API/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIM.API.HubConfig;
+using CIM.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+
+namespace CIM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : BoardcastController {
+
+        public DashboardController(
+        IResponseCacheService responseCacheService,
+        IHubContext<GlobalHub> hub,
+        IReportService service,
+        IConfiguration config,
+        IActiveProductionPlanService activeProductionPlanService
+        ) : base(hub, responseCacheService, service, config, activeProductionPlanService)
+        { }
+
+
+
+
+
+    }
+}
