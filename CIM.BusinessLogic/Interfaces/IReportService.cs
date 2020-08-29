2400790a00bdf59399a50139a0a13423664054f7
ToN NakuB <nprssp_tk@hotmail.com>
2020-08-29 13:36:25 +0700

    GetActiveMachineInfo

--- CIM.BusinessLogic/Interfaces/IReportService.cs
+++ CIM.BusinessLogic/Interfaces/IReportService.cs
@@ -12,6 +12,7 @@ namespace CIM.BusinessLogic.Interfaces {
         PagingModel<object> GetWasteHistory(string planId, int routeId, DateTime? from, DateTime? to, int page);
         Dictionary<string, int> GetActiveProductionPlanOutput();
         PagingModel<object> GetMachineStatusHistory(int howMany, int page, string planId, int routeId, int? machineId, DateTime? from = null, DateTime? to = null);
+        BoardcastDataModel GetActiveMachineInfo(string planId, int routeId, int machineId);
         DataTable GetOEEReport(ReportTimeCriteriaModel data);
         DataTable GetOutputReport(ReportTimeCriteriaModel data);
         DataTable GetWasteReport(ReportTimeCriteriaModel data);
