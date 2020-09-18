dafac9e21da03b8cea80f101accb55931833b76f
Ferine <sriwisai.a@gmail.com>
2020-09-18 15:34:10 +0700

    update app table

--- CIM.Domain/Models/App.cs
+++ CIM.Domain/Models/App.cs
@@ -14,6 +14,7 @@ namespace CIM.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public string ThUrl { get; set; }
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
 
