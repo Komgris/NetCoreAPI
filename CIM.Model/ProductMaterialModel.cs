41d61011d0b46919457a267d9a451fdd3e3b32e3
devspsec <devs.psec@gmail.com>
2020-11-04 07:59:25 +0700

    Add property to product material

--- CIM.Model/ProductMaterialModel.cs
+++ CIM.Model/ProductMaterialModel.cs
@@ -23,5 +23,6 @@ namespace CIM.Model
         public string MaterialDescription { get; set; }
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
-    }
+        public decimal QuantityUsage { get; set; }
+}
 }
