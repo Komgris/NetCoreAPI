aa99826c903140eb3e1f1e159938d1ee8a65cbd7
Itthipat <itthipat.cim@gmail.com>
2020-12-23 11:17:10 +0700

    change ProductionMasterShowModel

--- CIM.Model/ProductionMasterShowModel.cs
+++ CIM.Model/ProductionMasterShowModel.cs
@@ -10,7 +10,9 @@ namespace CIM.Model
         public string ProductCode { get; set; }
         public int MachineType_Id { get; set; }
         public string MachineName { get; set; }
+        public string MachineCode { get; set; }
         public string ShopNo { get; set; }
+        public double Target { get; set; }
         public decimal? Sequence { get; set; }
         public DateTime? ActualStart { get; set; }
         public DateTime? ActualFinish { get; set; }
